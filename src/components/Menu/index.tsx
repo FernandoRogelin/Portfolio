6687b437d47265d1b6133dbd5679d0831af174f2
Fernando Rogelin <fernando.rogelin@hotmail.com>
2023-09-30 16:45:04 -0300

    fix: Fix menu scroll layout

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,21 +1,39 @@
+import { useState, useEffect } from 'react'
 import './style.scss'
 
 import options from './options';
+import classNames from 'classnames';
 import Button from 'components/Button';
 import Logo from '../../assets/images/logo-preview.png';
 
 export default function Menu() {
+  const [scroll, setScroll] = useState(false)
+
+  function listenScrollEvent() {
+    window.scrollY < 20 ? setScroll(false) : setScroll(true)
+  }
+
+  useEffect(() => {
+    if (document.documentElement.scrollTop > 20) setScroll(true)
+
+    window.addEventListener('scroll', listenScrollEvent)
+
+    return () => window.removeEventListener('scroll', listenScrollEvent)
+  }, [])
+
   return (
-    <header className="menu">
-      <img className="menu__logo" src={Logo.src} alt="logo" />
-      <ul className="list">
-        {options.map((option, index) =>
-          <li key={index} className="list__item">{option.name}</li>
-        )}
-        <li>
-          <Button uppercase>contact</Button>
-        </li>
-      </ul>
+    <header className={classNames({ content: true, scroll })}>
+      <div className='menu'>
+        <img className="menu__logo" src={Logo.src} alt="logo" />
+        <ul className="list">
+          {options.map((option, index) =>
+            <li key={index} className="list__item">{option.name}</li>
+          )}
+          <li>
+            <Button uppercase>contact</Button>
+          </li>
+        </ul>
+      </div>
     </header>
   );
 }
