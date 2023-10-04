4751d4bb1cf7e6f538799a0cd47b879fce16a96e
Fernando Rogelin <fernando.rogelin@hotmail.com>
2023-10-04 15:49:45 -0300

    feat: Init services block

---
+++ src/layouts/Services/index.tsx
@@ -0,0 +1,11 @@
+import './styles.scss';
+
+const Services = () => {
+  return (
+    <div className='services'>
+      <h1 className='services__title'>Services</h1>
+    </div>
+  )
+}
+
+export default Services
